e3145059bff3f11f07a331655ef0380d283c9bc9
marcinSzym <marcin.szymainda@atos.net>
2024-01-17 13:38:27 +0100

    Add test

---
+++ src/main/java/com/example/springData/mydatabase/core/service/JobService.java
@@ -0,0 +1,9 @@
+package com.example.springData.mydatabase.core.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JobService {
+}
--- src/test/java/com/example/springData/jpa/demo/demo/DemoApplicationTests.java
+++ src/test/java/com/example/springData/jpa/demo/demo/DemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.springData.jpa.demo.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private JobService jobService;
 	@Test
-	void contextLoads() {
+	void test_get_all_job_with_jdbc_success() {
+		var result= jobService.getAllJobs();
+		Assertions.assertFalse(result.isEmpty());
 	}
 
 }
